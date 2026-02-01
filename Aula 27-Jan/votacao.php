98e33902255cc17bb2ab4bf32f7a07003114a8a8
GabrielFGuerreiro <gaferg2004@gmail.com>
2026-02-01 13:17:13 -0300

    Finalização da lógica condicional dos selecs na votação: separação em dois "form".

--- Aula 27-Jan/votacao.php
+++ Aula 27-Jan/votacao.php
@@ -1,6 +1,6 @@
 <?php
     $turmas = file("turmas.txt");
-    $alunos = file("alunos.txt");
+    $alunosArray = file("alunos.txt");
 ?>
 
 <!DOCTYPE html>
@@ -13,50 +13,42 @@
 </head>
 <body>
     <div class="container">
-        <?php if(!isset($_POST['turmaAluno']) && empty($_POST['turmaAluno'])) { ?>
-        <form id="formVotacao" action="votacao.php" method="post">
-            <label for="formVotacao">Escolha a turma</label>
-            <select name="turmaAluno" id="turmaAluno">
-            <option value="">Selecione...</option>
-            <?php foreach ($turmas as $turma): ?>
-                <option value="<?= $turma ?>"><?= $turma ?></option>
-            <?php endforeach; ?>
-            </select>
+        <!-- Formulário para selecionar a TURMA --><?php
+        if(empty($_POST['turmaAluno'])) { ?>
+            <form id="formVotacao" action="votacao.php" method="post">
+                <label for="formVotacao">Escolha a turma</label>
+                <select name="turmaAluno" id="turmaAluno" required>
+                    <option value="">Selecione...</option><?php
+                    foreach ($turmas as $turma): ?>
+                        <option value="<?= $turma ?>"><?= $turma ?></option><?php
+                    endforeach; ?>
+                </select>
 
-            <button type="submit" value="turmaAluno">Confirmar Seleção</button> <br><br><?php } ?>
+                <button type="submit" value="turmaAluno">Confirmar Seleção</button> <br><br>
+            </form><?php
+        } ?>
             
-            <?php if(isset($_POST['turmaAluno']) && !empty($_POST['turmaAluno'])) { ?>
-            <label for="aluno">Escolha seu voto para Representante</label>
-            <select name="aluno" id="aluno">
-            <option value="">Selecione...</option>
-            <?php
-                foreach ($alunos as $aluno): 
-                    $alunoPntr = explode(' | ', $aluno);
-
-                    if($alunoPntr[2] == $turmaSelect){ ?>
+        <!-- Formulário para selecionar o ALUNO --><?php 
+        if(!empty($_POST['turmaAluno'])) { ?>
+            <form action="">
+                <label for="aluno">Escolha seu voto para Representante</label>
+                <select name="aluno" id="aluno">
+                    <option value="">Selecione...</option><?php 
                     
-                    <option value="<?= $alunoPntr[0] ?>"> <?= $alunoPntr[0] ?> </option>
-            <?php } endforeach; ?>
-            </select>
-            <button type="submit" value="aluno">Confirmar Voto</button> <?php } ?>
+                    foreach ($alunosArray as $alunoLinha): 
+                        $alunoDados = explode(' | ', $alunoLinha);
+                        $turmaSelect = $_POST['turmaAluno'];
+                        
+                        if($alunoDados[2] == $turmaSelect): ?>
+                            <option value="<?= $alunoDados[0] ?>"> <?= $alunoDados[0] ?> </option><?php
+                        endif; ?><?php 
+                        
+                    endforeach; ?>
+                </select>
+                <button type="submit" value="aluno">Confirmar Voto</button>
+            </form><?php 
+        } ?>
 
-        </form>
     </div>
 </body>
 </html>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // $nomeAluno = str_contains($alunos, $turma)
-
-    $turmaSelect = $_POST['turmaAluno'];
-    echo "Turma selecionada: " . $turmaSelect;
-    echo $alunoPntr[0];
-}
-?>
-
-<!-- fazer com que haja uma comparação da string no turmas com alunos, 
-para que ele procura na linha para achar a string correspondente ao turmas
-exemplo: em turmas.txt tem a turma 'ads1' em alunos.txt, na quarta linha, 
-tem o aluno Walter, e na terceira coluna 'ads1', o codigo deve procurar pela string 'ads1' 
-e pegar somente o nome do aluno na linha. -->
